6f3dcac9042adb84b213dfb2799ca9755b999db7
mitch10593 <mitch@kilik.fr>
2020-02-27 22:07:38 +0100

    add unlock slotr on FARP London when Kutaisi is Captured

--- src/scripts/SimpleSlotBlockGameGUIConfig.lua
+++ src/scripts/SimpleSlotBlockGameGUIConfig.lua
@@ -8,5 +8,13 @@ trigger.action.setUserFlag("Kobuleti M-2000C Ford 2",100)
 trigger.action.setUserFlag("Kobuleti JF-17 Chevy 2",100)
 
 -- lock Senaki Airport
-
 trigger.action.setUserFlag("Senaki A-10C Enfield 2",100)
+
+-- lock FARP Paris
+trigger.action.setUserFlag("Paris SA342L Enfield 4",100)
+trigger.action.setUserFlag("Paris SA342M Enfield 5",100)
+trigger.action.setUserFlag("Paris SA342Minigun Enfield 6",100)
+trigger.action.setUserFlag("Paris Mi-8MTV2 Enfield 7",100)
+trigger.action.setUserFlag("Paris Mi-8MTV2 Enfield 8",100)
+trigger.action.setUserFlag("Paris Ka-50 Enfield 9",100)
+

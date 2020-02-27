d916e1639db4bfe6bb36dd277572c0e3c902d647
mitch10593 <mitch@kilik.fr>
2020-02-27 21:07:54 +0100

    try SimpleSlotblocker on Senaki and Kutaisi

---
+++ src/scripts/SimpleSlotBlockGameGUIConfig.lua
@@ -0,0 +1,12 @@
+-- enable SimpleSlotblocker
+
+trigger.action.setUserFlag("SSB",100)
+
+-- lock Kobuleti Airport
+trigger.action.setUserFlag("Kobuleti F-16C Dodge 2",100)
+trigger.action.setUserFlag("Kobuleti M-2000C Ford 2",100)
+trigger.action.setUserFlag("Kobuleti JF-17 Chevy 2",100)
+
+-- lock Senaki Airport
+
+trigger.action.setUserFlag("Senaki A-10C Enfield 2",100)
